c90cca243e5cd794ed4155605833d6128e5619c4
Gagan Chaudhary <singhgaganbtp@gmail.com>
2024-08-14 02:04:43 +0530

    setup for env

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv"
 import { DB_NAME } from "../constants.js";
 
+dotenv.config({path : "./.env"})
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected || DB HOST :${connectionInstance.connection.host}`)
 
     }
